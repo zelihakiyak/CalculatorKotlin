cc88d52dd551688dddbdb426f441008a5d78951e
zelihakiyak <zelihakyk64.43@hotmail.com>
2025-07-29 16:03:18 +0300

    init

--- app/src/main/java/com/zelihakiyak/calculatorkotlin/MainActivity.kt
+++ app/src/main/java/com/zelihakiyak/calculatorkotlin/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.zelihakiyak.calculatorkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
